144f53d014ec8457de4de18c9b787ac2c7135126
FranChesKo <dev.franchesko@gmail.com>
2024-06-12 22:54:23 +0800

    setup router and start http server

--- src/main.go
+++ src/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/franchesko/assembly-labyrinth/src/internal/config"
+import (
+	"log"
+	"net/http"
+
+	"github.com/franchesko/assembly-labyrinth/src/internal/api"
+	"github.com/franchesko/assembly-labyrinth/src/internal/config"
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
 
 func main() {
 	cfg := config.MustLoad()
-	_ = cfg
+
+	muxRouter := mux.NewRouter()
+	muxRouter.HandleFunc("/levels", api.GetLevelsHandler(cfg)).Methods("GET")
+	muxRouter.HandleFunc("/levels/{level}", api.GetLevelInfoHandler(cfg)).Methods("GET")
+	muxRouter.HandleFunc("/levels/{level}", api.GetRunLevelHandler(cfg)).Methods("POST")
+
+	router := cors.Default().Handler(muxRouter)
+	log.Fatal(http.ListenAndServe(cfg.Address+":"+cfg.Port, router))
 }
